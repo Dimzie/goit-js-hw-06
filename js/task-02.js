ab2e42389e677a9ba5d6af151eb731643c98747b
Dmytro Yankovskiy <dimayanik97@gmail.com>
2023-07-12 22:23:31 +0200

    Update task-02.js
    update

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const getIngredients = document.querySelector("#ingredients");
+console.log(getIngredients);
+
+const newIngredientsList = ingredients => {
+
+  return ingredients.map(element => {
+
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = element;
+
+  return itemEl;
+})};
+
+getIngredients.append(...newIngredientsList(ingredients));
